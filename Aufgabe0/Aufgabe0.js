98fc8cb18a194e47c89c7711c67c5d0251c1cd02
haasmanu <43847187+haasmanu@users.noreply.github.com>
2018-10-06 16:47:03 +0200

    new new

--- Aufgabe0/Aufgabe0.js
+++ Aufgabe0/Aufgabe0.js
@@ -2,7 +2,7 @@ function Funktion() {
     var Ergebnis = prompt("Sag mir deinen Namen");
     if (Ergebnis != null) {
         document.getElementById("solution").innerHTML =
-            "Uhhhh" + Ergebnis + " drueckt gerne auf Knoepfe!!!";
+            "Uhhhh " + Ergebnis + " drueckt gerne auf Knoepfe!!!";
     }
 }
 //# sourceMappingURL=Aufgabe0.js.map
