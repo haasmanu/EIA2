5729ae9e7ac7e71e836c92c39b7025df36cc4e5d
haasmanu <43847187+haasmanu@users.noreply.github.com>
2018-10-06 15:54:18 +0200

    boom

---
+++ Aufgabe0/Aufgabe0.js
@@ -0,0 +1,8 @@
+function Funktion() {
+    var Ergebnis = prompt("Ergebnis eingeben...");
+    if (Ergebnis != null) {
+        document.getElementById("solution").innerHTML =
+            Ergebnis + " minus 1 that`s 3, quick Maths...";
+    }
+}
+//# sourceMappingURL=Aufgabe0.js.map
---
+++ Aufgabe0/Aufgabe0.ts
@@ -0,0 +1,8 @@
+function Funktion(): void {
+var Ergebnis = prompt("Ergebnis eingeben...");
+
+if (Ergebnis != null) {
+    document.getElementById("solution").innerHTML =
+    Ergebnis + " minus 1 that`s 3, quick Maths...";
+    }
+}
