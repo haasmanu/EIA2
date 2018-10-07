10310ac80e2d99910123ef93be9df5b6c418268e
haasmanu <43847187+haasmanu@users.noreply.github.com>
2018-10-07 20:57:25 +0200

    new new

--- Aufgabe0/Aufgabe0.js
+++ Aufgabe0/Aufgabe0.js
@@ -4,5 +4,6 @@ function Funktion() {
         document.getElementById("solution").innerHTML =
             "Uhhhh " + Ergebnis + " na wie geht's dir?";
     }
+    console.log(Ergebnis + " schoen dich hier zu sehen.");
 }
 //# sourceMappingURL=Aufgabe0.js.map
--- Aufgabe0/Aufgabe0.ts
+++ Aufgabe0/Aufgabe0.ts
@@ -1,8 +1,10 @@
 function Funktion(): void {
-var Ergebnis = prompt("Sag mir deinen Namen");
+var Ergebnis : string = prompt("Sag mir deinen Namen");
 
 if (Ergebnis != null) {
     document.getElementById("solution").innerHTML =
     "Uhhhh "  +  Ergebnis + " na wie geht's dir?";
     }
+    
+    console.log(Ergebnis + " schoen dich hier zu sehen.")
 }
