a783449f59241f5de238f2d62afb1bb1d8c9879f
haasmanu <43847187+haasmanu@users.noreply.github.com>
2018-10-20 16:03:13 +0200

    erste Ansätze Aufgabe 2

---
+++ Aufgabe2/Aufgabe2UNO.js
@@ -0,0 +1,451 @@
+var Aufgabe2UNO;
+(function (Aufgabe2UNO) {
+    /* Sorry fuer das lange Interface, mir ist ein besserer Weg eingefallen, ab Zeile 664 wird es wieder spannend*/
+    /* red Cards */
+    let C0 = {
+        wert: "0",
+        farbe: "red"
+    };
+    let C1 = {
+        wert: "1",
+        farbe: "red"
+    };
+    let C2 = {
+        wert: "1",
+        farbe: "red"
+    };
+    let C3 = {
+        wert: "2",
+        farbe: "red"
+    };
+    let C4 = {
+        wert: "2",
+        farbe: "red"
+    };
+    let C5 = {
+        wert: "3",
+        farbe: "red"
+    };
+    let C6 = {
+        wert: "3",
+        farbe: "red"
+    };
+    let C7 = {
+        wert: "4",
+        farbe: "red"
+    };
+    let C8 = {
+        wert: "4",
+        farbe: "red"
+    };
+    let C9 = {
+        wert: "5",
+        farbe: "red"
+    };
+    let C10 = {
+        wert: "5",
+        farbe: "red"
+    };
+    let C11 = {
+        wert: "6",
+        farbe: "red"
+    };
+    let C12 = {
+        wert: "6",
+        farbe: "red"
+    };
+    let C13 = {
+        wert: "7",
+        farbe: "red"
+    };
+    let C14 = {
+        wert: "7",
+        farbe: "red"
+    };
+    let C15 = {
+        wert: "8",
+        farbe: "red"
+    };
+    let C16 = {
+        wert: "8",
+        farbe: "red"
+    };
+    let C17 = {
+        wert: "9",
+        farbe: "red"
+    };
+    let C18 = {
+        wert: "9",
+        farbe: "red"
+    };
+    let C19 = {
+        wert: "Aussetzen",
+        farbe: "red"
+    };
+    let C20 = {
+        wert: "Aussetzen",
+        farbe: "red"
+    };
+    let C21 = {
+        wert: "Wechsel",
+        farbe: "red"
+    };
+    let C22 = {
+        wert: "Wechsel",
+        farbe: "red"
+    };
+    let C23 = {
+        wert: "2 mehr",
+        farbe: "red"
+    };
+    let C24 = {
+        wert: "2 mehr",
+        farbe: "red"
+    };
+    /*green Cards */
+    let C25 = {
+        wert: "0",
+        farbe: "green"
+    };
+    let C26 = {
+        wert: "1",
+        farbe: "green"
+    };
+    let C27 = {
+        wert: "1",
+        farbe: "green"
+    };
+    let C28 = {
+        wert: "2",
+        farbe: "green"
+    };
+    let C29 = {
+        wert: "2",
+        farbe: "green"
+    };
+    let C30 = {
+        wert: "3",
+        farbe: "green"
+    };
+    let C31 = {
+        wert: "3",
+        farbe: "green"
+    };
+    let C32 = {
+        wert: "4",
+        farbe: "green"
+    };
+    let C33 = {
+        wert: "4",
+        farbe: "green"
+    };
+    let C34 = {
+        wert: "5",
+        farbe: "green"
+    };
+    let C35 = {
+        wert: "5",
+        farbe: "green"
+    };
+    let C36 = {
+        wert: "6",
+        farbe: "green"
+    };
+    let C37 = {
+        wert: "6",
+        farbe: "green"
+    };
+    let C38 = {
+        wert: "7",
+        farbe: "green"
+    };
+    let C39 = {
+        wert: "7",
+        farbe: "green"
+    };
+    let C40 = {
+        wert: "8",
+        farbe: "green"
+    };
+    let C41 = {
+        wert: "8",
+        farbe: "green"
+    };
+    let C42 = {
+        wert: "9",
+        farbe: "green"
+    };
+    let C43 = {
+        wert: "9",
+        farbe: "green"
+    };
+    let C44 = {
+        wert: "Aussetzen",
+        farbe: "green"
+    };
+    let C45 = {
+        wert: "Aussetzen",
+        farbe: "green"
+    };
+    let C46 = {
+        wert: "Wechsel",
+        farbe: "green"
+    };
+    let C47 = {
+        wert: "Wechsel",
+        farbe: "green"
+    };
+    let C48 = {
+        wert: "2 mehr",
+        farbe: "green"
+    };
+    let C49 = {
+        wert: "2 mehr",
+        farbe: "green"
+    };
+    /* blue Cards */
+    let C50 = {
+        wert: "0",
+        farbe: "blue"
+    };
+    let C51 = {
+        wert: "1",
+        farbe: "blue"
+    };
+    let C52 = {
+        wert: "1",
+        farbe: "blue"
+    };
+    let C53 = {
+        wert: "2",
+        farbe: "blue"
+    };
+    let C54 = {
+        wert: "2",
+        farbe: "blue"
+    };
+    let C55 = {
+        wert: "3",
+        farbe: "blue"
+    };
+    let C56 = {
+        wert: "3",
+        farbe: "blue"
+    };
+    let C57 = {
+        wert: "4",
+        farbe: "blue"
+    };
+    let C58 = {
+        wert: "4",
+        farbe: "blue"
+    };
+    let C59 = {
+        wert: "5",
+        farbe: "blue"
+    };
+    let C60 = {
+        wert: "5",
+        farbe: "blue"
+    };
+    let C61 = {
+        wert: "6",
+        farbe: "blue"
+    };
+    let C62 = {
+        wert: "6",
+        farbe: "blue"
+    };
+    let C63 = {
+        wert: "7",
+        farbe: "blue"
+    };
+    let C64 = {
+        wert: "7",
+        farbe: "blue"
+    };
+    let C65 = {
+        wert: "8",
+        farbe: "blue"
+    };
+    let C66 = {
+        wert: "8",
+        farbe: "blue"
+    };
+    let C67 = {
+        wert: "9",
+        farbe: "blue"
+    };
+    let C68 = {
+        wert: "9",
+        farbe: "blue"
+    };
+    let C69 = {
+        wert: "Aussetzen",
+        farbe: "blue"
+    };
+    let C70 = {
+        wert: "Aussetzen",
+        farbe: "blue"
+    };
+    let C71 = {
+        wert: "Wechsel",
+        farbe: "blue"
+    };
+    let C72 = {
+        wert: "Wechsel",
+        farbe: "blue"
+    };
+    let C73 = {
+        wert: "2 mehr",
+        farbe: "blue"
+    };
+    let C74 = {
+        wert: "2 mehr",
+        farbe: "blue"
+    };
+    /* yellow Cards */
+    let C75 = {
+        wert: "0",
+        farbe: "yellow"
+    };
+    let C76 = {
+        wert: "1",
+        farbe: "yellow"
+    };
+    let C77 = {
+        wert: "1",
+        farbe: "yellow"
+    };
+    let C78 = {
+        wert: "2",
+        farbe: "yellow"
+    };
+    let C79 = {
+        wert: "2",
+        farbe: "yellow"
+    };
+    let C80 = {
+        wert: "3",
+        farbe: "yellow"
+    };
+    let C81 = {
+        wert: "3",
+        farbe: "yellow"
+    };
+    let C82 = {
+        wert: "4",
+        farbe: "yellow"
+    };
+    let C83 = {
+        wert: "4",
+        farbe: "yellow"
+    };
+    let C84 = {
+        wert: "5",
+        farbe: "yellow"
+    };
+    let C85 = {
+        wert: "5",
+        farbe: "yellow"
+    };
+    let C86 = {
+        wert: "6",
+        farbe: "yellow"
+    };
+    let C87 = {
+        wert: "6",
+        farbe: "yellow"
+    };
+    let C88 = {
+        wert: "7",
+        farbe: "yellow"
+    };
+    let C89 = {
+        wert: "7",
+        farbe: "yellow"
+    };
+    let C90 = {
+        wert: "8",
+        farbe: "yellow"
+    };
+    let C91 = {
+        wert: "8",
+        farbe: "yellow"
+    };
+    let C92 = {
+        wert: "9",
+        farbe: "yellow"
+    };
+    let C93 = {
+        wert: "9",
+        farbe: "yellow"
+    };
+    let C94 = {
+        wert: "Aussetzen",
+        farbe: "yellow"
+    };
+    let C95 = {
+        wert: "Aussetzen",
+        farbe: "yellow"
+    };
+    let C96 = {
+        wert: "Wechsel",
+        farbe: "yellow"
+    };
+    let C97 = {
+        wert: "Wechsel",
+        farbe: "yellow"
+    };
+    let C98 = {
+        wert: "2 mehr",
+        farbe: "yellow"
+    };
+    let C99 = {
+        wert: "2 mehr",
+        farbe: "yellow"
+    };
+    /* special Cards */
+    let C100 = {
+        wert: "4 mehr",
+        farbe: "black"
+    };
+    let C101 = {
+        wert: "4 mehr",
+        farbe: "black"
+    };
+    let C102 = {
+        wert: "4 mehr",
+        farbe: "black"
+    };
+    let C103 = {
+        wert: "4 mehr",
+        farbe: "black"
+    };
+    let C104 = {
+        wert: "Wish Card",
+        farbe: "black"
+    };
+    let C105 = {
+        wert: "Wish Card",
+        farbe: "black"
+    };
+    let C106 = {
+        wert: "Wish Card",
+        farbe: "black"
+    };
+    let C107 = {
+        wert: "Wish Card",
+        farbe: "black"
+    };
+    let STAPEL = [C0, C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12, C13, C14, C15, C16, C17, C18, C19, C20, C21, C22, C23, C24, C25,
+        C26, C27, C28, C29, C30, C31, C32, C33, C34, C35, C36, C37, C38, C39, C40, C41, C42, C43, C44, C45, C46, C47, C48, C49,
+        C50, C51, C52, C53, C54, C55, C56, C57, C58, C59, C60, C61, C62, C63, C64, C65, C66, C67, C68, C69, C70, C71, C72, C73,
+        C74, C75, C76, C77, C78, C79, C80, C81, C82, C83, C84, C85, C86, C87, C88, C89, C90, C91, C92, C93, C94, C95, C96, C97,
+        C98, C99, C100, C101, C102, C103, C104, C105, C106, C107];
+    /* Random Number*/
+    function randNumber(i) {
+        let n = Math.floor(Math.random() * 107);
+    }
+})(Aufgabe2UNO || (Aufgabe2UNO = {}));
+//# sourceMappingURL=Aufgabe2UNO.js.map
