322a058d95a1b29de14a0b55faa80fb3e7d66851
haasmanu <43847187+haasmanu@users.noreply.github.com>
2018-11-06 15:22:38 +0100

    Verbesserte und gepüfte Version

--- Aufgabe2/Aufgabe2UNO.js
+++ Aufgabe2/Aufgabe2UNO.js
@@ -2,32 +2,32 @@ var Aufgabe2UNO;
 (function (Aufgabe2UNO) {
     document.addEventListener("DOMContentLoaded", main);
     let hand = [];
-    let stapel = [
-        { wert: "0", farbe: "red" }, { wert: "1", farbe: "red" }, { wert: "1", farbe: "red" }, { wert: "2", farbe: "red" }, { wert: "2", farbe: "red" }, { wert: "3", farbe: "red" }, { wert: "3", farbe: "red" }, { wert: "4", farbe: "red" }, { wert: "4", farbe: "red" }, { wert: "5", farbe: "red" }, { wert: "5", farbe: "red" },
-        { wert: "6", farbe: "red" }, { wert: "6", farbe: "red" }, { wert: "7", farbe: "red" }, { wert: "7", farbe: "red" }, { wert: "8", farbe: "red" }, { wert: "8", farbe: "red" }, { wert: "9", farbe: "red" }, { wert: "9", farbe: "red" }, { wert: "Aussetzen", farbe: "red" }, { wert: "Aussetzen", farbe: "red" }, { wert: "2 mehr", farbe: "red" }, { wert: "2 mehr", farbe: "red" }, { wert: "Wechsel", farbe: "red" }, { wert: "Wechsel", farbe: "red" },
-        { wert: "0", farbe: "green" }, { wert: "1", farbe: "green" }, { wert: "1", farbe: "green" }, { wert: "2", farbe: "green" }, { wert: "2", farbe: "green" }, { wert: "3", farbe: "green" }, { wert: "3", farbe: "green" }, { wert: "4", farbe: "green" }, { wert: "4", farbe: "green" }, { wert: "5", farbe: "green" }, { wert: "5", farbe: "green" },
-        { wert: "6", farbe: "green" }, { wert: "6", farbe: "green" }, { wert: "7", farbe: "green" }, { wert: "7", farbe: "green" }, { wert: "8", farbe: "green" }, { wert: "8", farbe: "green" }, { wert: "9", farbe: "green" }, { wert: "9", farbe: "green" }, { wert: "Aussetzen", farbe: "green" }, { wert: "Aussetzen", farbe: "green" }, { wert: "2 mehr", farbe: "green" }, { wert: "2 mehr", farbe: "green" }, { wert: "Wechsel", farbe: "green" }, { wert: "Wechsel", farbe: "green" },
-        { wert: "0", farbe: "blue" }, { wert: "1", farbe: "blue" }, { wert: "1", farbe: "blue" }, { wert: "2", farbe: "blue" }, { wert: "2", farbe: "blue" }, { wert: "3", farbe: "blue" }, { wert: "3", farbe: "blue" }, { wert: "4", farbe: "blue" }, { wert: "4", farbe: "blue" }, { wert: "5", farbe: "blue" }, { wert: "5", farbe: "blue" },
-        { wert: "6", farbe: "blue" }, { wert: "6", farbe: "blue" }, { wert: "7", farbe: "blue" }, { wert: "7", farbe: "blue" }, { wert: "8", farbe: "blue" }, { wert: "8", farbe: "blue" }, { wert: "9", farbe: "blue" }, { wert: "9", farbe: "blue" }, { wert: "Aussetzen", farbe: "blue" }, { wert: "Aussetzen", farbe: "blue" }, { wert: "2 mehr", farbe: "blue" }, { wert: "2 mehr", farbe: "blue" }, { wert: "Wechsel", farbe: "blue" }, { wert: "Wechsel", farbe: "blue" },
-        { wert: "0", farbe: "yellow" }, { wert: "1", farbe: "yellow" }, { wert: "1", farbe: "yellow" }, { wert: "2", farbe: "yellow" }, { wert: "2", farbe: "yellow" }, { wert: "3", farbe: "yellow" }, { wert: "3", farbe: "yellow" }, { wert: "4", farbe: "yellow" }, { wert: "4", farbe: "yellow" }, { wert: "5", farbe: "yellow" }, { wert: "5", farbe: "yellow" },
-        { wert: "6", farbe: "yellow" }, { wert: "6", farbe: "yellow" }, { wert: "7", farbe: "yellow" }, { wert: "7", farbe: "yellow" }, { wert: "8", farbe: "yellow" }, { wert: "8", farbe: "yellow" }, { wert: "9", farbe: "yellow" }, { wert: "9", farbe: "yellow" }, { wert: "Aussetzen", farbe: "yellow" }, { wert: "Aussetzen", farbe: "yellow" }, { wert: "2 mehr", farbe: "yellow" }, { wert: "2 mehr", farbe: "yellow" }, { wert: "Wechsel", farbe: "yellow" }, { wert: "Wechsel", farbe: "yellow" },
-        { wert: "Wish Card", farbe: "black" }, { wert: "Wish Card", farbe: "black" }, { wert: "Wish Card", farbe: "black" }, { wert: "Wish Card", farbe: "black" }, { wert: "4 mehr", farbe: "black" }, { wert: "4 mehr", farbe: "black" }, { wert: "4 mehr", farbe: "black" }, { wert: "4 mehr", farbe: "black" }];
+    let deck = [
+        { value: "0", color: "red" }, { value: "1", color: "red" }, { value: "1", color: "red" }, { value: "2", color: "red" }, { value: "2", color: "red" }, { value: "3", color: "red" }, { value: "3", color: "red" }, { value: "4", color: "red" }, { value: "4", color: "red" }, { value: "5", color: "red" }, { value: "5", color: "red" },
+        { value: "6", color: "red" }, { value: "6", color: "red" }, { value: "7", color: "red" }, { value: "7", color: "red" }, { value: "8", color: "red" }, { value: "8", color: "red" }, { value: "9", color: "red" }, { value: "9", color: "red" }, { value: "Aussetzen", color: "red" }, { value: "Aussetzen", color: "red" }, { value: "2 mehr", color: "red" }, { value: "2 mehr", color: "red" }, { value: "Wechsel", color: "red" }, { value: "Wechsel", color: "red" },
+        { value: "0", color: "green" }, { value: "1", color: "green" }, { value: "1", color: "green" }, { value: "2", color: "green" }, { value: "2", color: "green" }, { value: "3", color: "green" }, { value: "3", color: "green" }, { value: "4", color: "green" }, { value: "4", color: "green" }, { value: "5", color: "green" }, { value: "5", color: "green" },
+        { value: "6", color: "green" }, { value: "6", color: "green" }, { value: "7", color: "green" }, { value: "7", color: "green" }, { value: "8", color: "green" }, { value: "8", color: "green" }, { value: "9", color: "green" }, { value: "9", color: "green" }, { value: "Aussetzen", color: "green" }, { value: "Aussetzen", color: "green" }, { value: "2 mehr", color: "green" }, { value: "2 mehr", color: "green" }, { value: "Wechsel", color: "green" }, { value: "Wechsel", color: "green" },
+        { value: "0", color: "blue" }, { value: "1", color: "blue" }, { value: "1", color: "blue" }, { value: "2", color: "blue" }, { value: "2", color: "blue" }, { value: "3", color: "blue" }, { value: "3", color: "blue" }, { value: "4", color: "blue" }, { value: "4", color: "blue" }, { value: "5", color: "blue" }, { value: "5", color: "blue" },
+        { value: "6", color: "blue" }, { value: "6", color: "blue" }, { value: "7", color: "blue" }, { value: "7", color: "blue" }, { value: "8", color: "blue" }, { value: "8", color: "blue" }, { value: "9", color: "blue" }, { value: "9", color: "blue" }, { value: "Aussetzen", color: "blue" }, { value: "Aussetzen", color: "blue" }, { value: "2 mehr", color: "blue" }, { value: "2 mehr", color: "blue" }, { value: "Wechsel", color: "blue" }, { value: "Wechsel", color: "blue" },
+        { value: "0", color: "yellow" }, { value: "1", color: "yellow" }, { value: "1", color: "yellow" }, { value: "2", color: "yellow" }, { value: "2", color: "yellow" }, { value: "3", color: "yellow" }, { value: "3", color: "yellow" }, { value: "4", color: "yellow" }, { value: "4", color: "yellow" }, { value: "5", color: "yellow" }, { value: "5", color: "yellow" },
+        { value: "6", color: "yellow" }, { value: "6", color: "yellow" }, { value: "7", color: "yellow" }, { value: "7", color: "yellow" }, { value: "8", color: "yellow" }, { value: "8", color: "yellow" }, { value: "9", color: "yellow" }, { value: "9", color: "yellow" }, { value: "Aussetzen", color: "yellow" }, { value: "Aussetzen", color: "yellow" }, { value: "2 mehr", color: "yellow" }, { value: "2 mehr", color: "yellow" }, { value: "Wechsel", color: "yellow" }, { value: "Wechsel", color: "yellow" },
+        { value: "Wish Card", color: "black" }, { value: "Wish Card", color: "black" }, { value: "Wish Card", color: "black" }, { value: "Wish Card", color: "black" }, { value: "4 mehr", color: "black" }, { value: "4 mehr", color: "black" }, { value: "4 mehr", color: "black" }, { value: "4 mehr", color: "black" }];
     function main() {
         let y = prompt("Gib Anzahl der Karten an");
-        let x;
-        x = Number(y);
-        for (let i = 0; i < x; i++) {
-            let r = Math.floor(Math.random() * stapel.length);
-            let randCard = stapel[r];
-            console.log(randCard);
-            hand[i] = randCard;
-            stapel.splice(r, 1);
+        let wantedCards;
+        wantedCards = Number(y);
+        for (let i = 0; i < wantedCards; i++) {
+            let r = Math.floor(Math.random() * deck.length);
+            let randomCard = deck[r];
+            console.log(randomCard);
+            hand[i] = randomCard;
+            deck.splice(r, 1);
         }
         displayCards();
     }
     function displayCards() {
         for (let j = 0; j < hand.length; j++) {
-            placeCards(hand[j].wert, hand[j].farbe);
+            placeCards(hand[j].value, hand[j].color);
         }
     }
     function placeCards(_wert, _farbe) {
