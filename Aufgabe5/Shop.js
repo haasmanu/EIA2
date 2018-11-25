034b81ecf16ab474f4c1e03846112fcaf4576923
haasmanu <43847187+haasmanu@users.noreply.github.com>
2018-11-25 13:44:54 +0100

    Aufgabe 5

---
+++ Aufgabe5/Shop.js
@@ -0,0 +1,32 @@
+var Aufgabe5Shop;
+(function (Aufgabe5Shop) {
+    Aufgabe5Shop.tree = [{ name: "Fichte", price: 20 },
+        { name: "Tanne", price: 18 },
+        { name: "Oregon Pine", price: 22 }];
+    Aufgabe5Shop.treeBall = [{ name: "None", price: 0 },
+        { name: "Red-Balls", price: 0.99 },
+        { name: "Gold-Balls", price: 1.49 },
+        { name: "Silver-Balls", price: 1.99 }];
+    Aufgabe5Shop.candle = [{ name: "None", price: 0 },
+        { name: "Red-Candels", price: 0.49 },
+        { name: "White-Candels", price: 0.99 },
+        { name: "Blue-Candels", price: 1.49 }];
+    Aufgabe5Shop.lametta = [{ name: "None", price: 0 },
+        { name: "Lametta White", price: 0.5 },
+        { name: "Lametta Silver", price: 1 },
+        { name: "Lametta Gold", price: 2 }];
+    Aufgabe5Shop.treeStand = [{ name: "None", price: 0 },
+        { name: "Star", price: 20 },
+        { name: "Classic", price: 15 }];
+    Aufgabe5Shop.additional = [{ name: "None", price: 0 },
+        { name: "Star", price: 3 },
+        { name: "Train", price: 5 },
+        { name: "Angel", price: 6 },
+        { name: "Lil $anta", price: 8 },
+        { name: "Yung Je$u$", price: 10 }];
+    Aufgabe5Shop.shipment = [{ name: "DHL", price: 25 },
+        { name: "Hermes", price: 20 },
+        { name: "Express", price: 50 },
+        { name: "Abholung", price: 0 }];
+})(Aufgabe5Shop || (Aufgabe5Shop = {}));
+//# sourceMappingURL=Shop.js.map
---
+++ Aufgabe5/Shop.ts
@@ -0,0 +1,42 @@
+namespace Aufgabe5Shop {
+
+    export let tree: Product[] = [{ name: "Fichte", price: 20}, 
+                            { name: "Tanne", price: 18}, 
+                            { name: "Oregon Pine", price: 22}];
+    
+    export let treeBall: Product[] = [{ name: "None", price: 0},
+                                { name: "Red-Balls", price: 0.99}, 
+                                { name: "Gold-Balls", price: 1.49}, 
+                                { name: "Silver-Balls", price: 1.99}];
+    
+    export let candle: Product[] = [{ name: "None", price: 0},
+                                { name: "Red-Candels", price: 0.49},
+                                { name: "White-Candels", price: 0.99},
+                                { name: "Blue-Candels", price: 1.49}];
+    
+    export let lametta: Product[] = [{ name: "None", price: 0},
+                                { name: "Lametta White", price: 0.5},
+                                { name: "Lametta Silver", price: 1},
+                                { name: "Lametta Gold", price: 2}];
+    
+    export let treeStand: Product[] = [{ name: "None" , price: 0},
+                                { name: "Star", price: 20},
+                                { name: "Classic", price: 15}];
+    
+    export let additional: Product[] = [{ name: "None" , price: 0},
+                                    { name: "Star", price: 3},
+                                    { name: "Train", price: 5},
+                                    { name: "Angel", price: 6},
+                                    { name: "Lil $anta", price: 8},
+                                    { name: "Yung Je$u$", price: 10}]; 
+     
+    export let shipment: Product[] = [{ name: "DHL" , price: 25},
+                                       { name: "Hermes", price: 20},
+                                       { name: "Express" , price: 50},
+                                       { name: "Abholung" , price: 0}];  
+
+export interface Product {
+        name: string;
+        price: number;
+    }   
+}
